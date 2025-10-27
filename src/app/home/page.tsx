001c67de36f4e31533d047dd036d742371eeb92f
Your Name <your@email.com>
2025-10-27 19:33:01 +0545

    google login added

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,30 @@
 "use client"
-import {signIn} from "next-auth/react"
+import {signIn,signOut,useSession} from "next-auth/react"
 import Navbar from "../navbar/page"
+import Image from "next/image";
+
 
 
 function home(){
+    const {data:session} =useSession()
+    if(session){
+        return(
+//  google integration for logout code
+            <>
+            <Image src={session.user?.image || "mero-profile.png"} alt="profile picture" width={80} height={80} /> 
+        <h1>Welcome,{session.user?.name}</h1> 
+        <h3>{session.user?.email}</h3>
+        <button onClick={()=>signOut()}>Sign Out</button>
+        </>
+        )
+    }
     return(
+        // google integration for login
        <div>
-        <Navbar/>
+        <h1>Not Logged In</h1>
         <button onClick ={()=>signIn("google")}>sign in with google</button>
        </div>
+       
     )
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import SessionWrapper from "./components/SessionWrapper";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,13 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <SessionWrapper>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        >
         
         {children}
       </body>
     </html>
+        </SessionWrapper>
   );
 }
